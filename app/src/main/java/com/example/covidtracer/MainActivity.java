b93ae39d6d7a5860f05b984ef1245d072fdfd7c9
ZIDAN <zidanali111@gmail.com>
2022-02-03 23:16:40 +0530

    CountryData and ApiInterface committed.

--- app/src/main/java/com/example/covidtracer/MainActivity.java
+++ app/src/main/java/com/example/covidtracer/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.covidtracer;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView totalConfirm;
+    private TextView totalActive;
+    private TextView totalRecovered;
+    private TextView totalDeath;
+    private TextView totalTests;
+    private TextView todayConfirm;
+    private TextView todayRecovered;
+    private TextView todayDeath;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
 
+    }
 
+
+    private void init() {
+        totalConfirm = findViewById(R.id.totalConfirm);
+        totalActive = findViewById(R.id.totalActive);
+        totalRecovered = findViewById(R.id.totalRecovered);
+        totalDeath = findViewById(R.id.totalDeath);
+        todayConfirm = findViewById(R.id.todayConfirm);
+        todayRecovered = findViewById(R.id.todayRecovered);
+        todayDeath = findViewById(R.id.todayDeath);
     }
 }
---
+++ app/src/main/java/com/example/covidtracer/api/ApiUtilities.java
@@ -0,0 +1,6 @@
+package com.example.covidtracer.api;
+
+
+public class ApiUtilities {
+
+}
